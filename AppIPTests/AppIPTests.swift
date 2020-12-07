539a3a11200ab2658297efa8b086e3590566fb9d
mcan <mcan@gmx.fr>
2020-12-07 04:41:28 +0100

    Initial Commit

---
+++ AppIPTests/AppIPTests.swift
@@ -0,0 +1,33 @@
+//
+//  AppIPTests.swift
+//  AppIPTests
+//
+//  Created by Marc on 2020/12/07.
+//
+
+import XCTest
+@testable import AppIP
+
+class AppIPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
